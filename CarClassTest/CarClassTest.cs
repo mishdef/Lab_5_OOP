fb5ab63ca978a8b8f4e7f890a08b16cf731734d4
Michailo Stadnikov <stadnmisha@gmail.com>
2025-10-01 12:13:59 +0300

    Created a regular new project CarClassTest with MSTest

---
+++ CarClassTest/CarClassTest.cs
@@ -0,0 +1,14 @@
+﻿using Lab_5;
+
+namespace CarClassTest
+{
+    [TestClass]
+    public sealed class CarClassTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
